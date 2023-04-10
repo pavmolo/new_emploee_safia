6f5ea98af63af81973a0330079af327188cb3a92
pavmolo <74321783+pavmolo@users.noreply.github.com>
2023-04-10 07:09:10 +0300

    Update new_emploee_dafia_main.py

--- new_emploee_dafia_main.py
+++ new_emploee_dafia_main.py
@@ -2,7 +2,6 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 import pickle
-import catboost
 from catboost import CatBoostRegressor
 
 def create_new_dict(sex, age, fam, childrens, ed):
