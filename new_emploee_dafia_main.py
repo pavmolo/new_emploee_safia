6a822b63a90716b29dd44917e35f36f69c8bb842
pavmolo <74321783+pavmolo@users.noreply.github.com>
2023-04-12 09:28:35 +0300

    Update new_emploee_dafia_main.py

--- new_emploee_dafia_main.py
+++ new_emploee_dafia_main.py
@@ -10,11 +10,6 @@ def create_new_dict(sex, age, fam, childrens, ed):
     num_input = []
     num_input.append(age)
     
-    if sex == "Мужчина":
-        num_input.append(1)
-    else:
-        num_input.append(0)
-        
     num_input.append(childrens)
     
     if fam == "Холост / Не замужем":
@@ -29,7 +24,12 @@ def create_new_dict(sex, age, fam, childrens, ed):
     elif ed == 'Среднее':
         num_input.append(2)
     else:
-        num_input.append(3)    
+        num_input.append(3)
+        
+    if sex == "Мужчина":
+        num_input.append(1)
+    else:
+        num_input.append(0)
     
     return num_input
 
